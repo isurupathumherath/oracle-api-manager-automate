11480438a7856ce84221cdca6a08052c6997ae6b
Isuru Pathum Herath <77333662+isurupathumherath@users.noreply.github.com>
2022-06-16 10:59:01 +0530

    initial Commit

---
+++ src/main/java/com/lolctech/apimanagerautomate/ApiManagerAutomateApplication.java
@@ -0,0 +1,13 @@
+package com.lolctech.apimanagerautomate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiManagerAutomateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiManagerAutomateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lolctech/apimanagerautomate/ApiManagerAutomateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lolctech.apimanagerautomate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiManagerAutomateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
